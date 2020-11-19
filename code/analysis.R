444c4b7c8ad3a37bb327ce2279f245009d70d8ef
Johannes <johannespein@yahoo.de>
2020-11-19 16:22:24 +0100

    update analysis.R, clean tidy.R

--- code/analysis.R
+++ code/analysis.R
@@ -1,4 +1,75 @@
+library(randomForest)
+library(dplyr)
+library(ggplot2) 
+
+tidy_ikea # make sure that tidy.R is executed and tidy_ikea is loaded into memory
+
 # ANALYSIS
+
+# correlation price + old_price
+
+cor.test(tidy_ikea$price_eur, tidy_ikea$old_price_eur, 
+         method = "pearson")
+
+# importance - drop_na
+rf_ikea <- tidy_ikea %>%
+  select(-old_price_eur) %>%
+  drop_na()
+
+rf_ikea$designer <- fct_lump_n(rf_ikea$designer, 51)
+rf_ikea$name <- fct_lump_n(rf_ikea$name, 51)
+
+ikea.rf <- randomForest(price_eur ~ ., rf_ikea, ntree=2000,
+                          keep.forest=FALSE, importance=TRUE)
+imp_drop_na <- importance(ikea.rf)
+
+# importance - dummy value instead of na
+
+rf_ikea <- tidy_ikea 
+
+rf_ikea$size_m3[is.na(rf_ikea$size_m3)] <- -1000
+
+rf_ikea <- rf_ikea %>%
+  select(-old_price_eur) %>%
+  drop_na()
+
+rf_ikea$designer <- fct_lump_n(rf_ikea$designer, 51)
+rf_ikea$name <- fct_lump_n(rf_ikea$name, 51)
+
+ikea.rf <- randomForest(price_eur ~ ., rf_ikea, ntree=2000,
+                        keep.forest=FALSE, importance=TRUE)
+imp_size_minus_1000 <- importance(ikea.rf)
+
+
+# importance - na roughfix
+
+rf_ikea <- tidy_ikea %>%
+  select(-old_price_eur)
+
+rf_ikea$designer <- fct_lump_n(rf_ikea$designer, 51)
+rf_ikea$name <- fct_lump_n(rf_ikea$name, 51)
+
+ikea.rf <- randomForest(price_eur ~ ., rf_ikea, ntree=2000,
+                        keep.forest=FALSE, importance=TRUE, na.action = na.roughfix)
+imp_na_roughfix <- importance(ikea.rf)
+
+
+# mean importance
+mean_imp <- as.data.frame((imp_na_roughfix+imp_size_minus_1000+imp_drop_na)/3)
+
+mean_imp$varnames <- rownames(mean_imp)
+rownames(mean_imp) <- NULL
+mean_imp <- mean_imp %>%
+  rename('PercentIncMSE' = '%IncMSE')
+
+ggplot(mean_imp, aes(y=reorder(varnames, PercentIncMSE), x=PercentIncMSE)) + 
+  geom_point() +
+  labs(x = "% Increase MSE", y = "Feature", title = "Mean Feature Importance")
+
+
+
+
+# ANALYSIS_OLD
 
 tidy_ikea # make sure that ikea.R is executed and tidy_ikea is loaded into memory
 
@@ -50,14 +121,12 @@ tidy_ikea %>%
 
 # material cost
 
-
-
-
-
 # model, tbd
 tidy_ikea %>%
-  lm(price_eur ~ designer + size_m3 + category, data = .) %>%
+  lm(price_eur ~ name + category + price_eur + old_price_eur + sellable_online + other_colors + designer + size_m3, data = .) %>%
   summary()
+
+
 
 
 
--- code/tidy.R
+++ code/tidy.R
@@ -1,6 +1,6 @@
 library(tidyverse)
 library(readr)
-ikea <- read_delim("ikea.csv", ";", 
+ikea <- read_delim("./ikea.csv", ";", 
                    escape_double = FALSE, 
                    trim_ws = TRUE,
                    col_types = cols(
@@ -17,23 +17,23 @@ tidy_ikea <- ikea
 
 tidy_ikea$designer[grepl("\\d",tidy_ikea$designer)]<- NA
 
-
 tidy_ikea <- tidy_ikea %>%
   separate(designer, c("d1", "d2", "d3", "d4", "d5", "d6", "d7"), sep = "/")
 
-tidy_ikea[ , 11:17][tidy_ikea[ , 11:17] == "IKEA of Sweden" ] <- NA
+#tidy_ikea[ , 11:17][tidy_ikea[ , 11:17] == "IKEA of Sweden" ] <- NA
 tidy_ikea[ , 11:17] <- t(apply(tidy_ikea[ , 11:17], 1, function(x) c(sort(x[x!='']), x[x==''])))
 
 tidy_ikea <- tidy_ikea %>%
   unite(col = "designer", d1, d2, d3, d4, d5, d6, d7, sep = ", ", na.rm = TRUE)
 
 tidy_ikea$designer[tidy_ikea$designer == ""] <- NA
+tidy_ikea$designer <- factor(tidy_ikea$designer)
 
-## convert other colors to boolean
+## tidy other_colors
 tidy_ikea <- tidy_ikea %>%
   mutate(other_colors = other_colors == "Yes")
 
-## convert old price to integer and times 10
+## tidy_old_price
 tidy_ikea <- tidy_ikea %>%
   mutate(old_price = str_replace(old_price, "SR ", "")) %>%
   mutate(old_price = str_replace_all(old_price, ",", "")) %>%
@@ -42,15 +42,14 @@ tidy_ikea <- tidy_ikea %>%
   
 # transform data columns
 
-# add size in m^3
+## add size in m^3
 tidy_ikea <- tidy_ikea %>%
   mutate(size_m3 = round(depth/100 * width/100 * height/100, 2))
 
-# transform price and old price in eur
+## transform price and old_price to euro
 tidy_ikea <- tidy_ikea %>%
   mutate(price_eur = round(price * 0.23 / 10, 2), old_price_eur = round(old_price * 0.23 / 10, 2))
 
-## select relevant data
+# select relevant data for further exploration
 tidy_ikea <- tidy_ikea %>%
   select(name, category, price_eur, old_price_eur, sellable_online, other_colors, designer, size_m3)
-
